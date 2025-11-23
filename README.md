01bd9c040d4424aae25b84d003158201f03876e1
piyush.1 <piyush.1@infoedge.com>
2025-11-24 00:20:19 +0530

    setup login page

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- Setup project with vite react.
+- Remove unwanted files
+- Install Tailwind
+- Install dasiy ui
+- add NavBar daisy ui component
+- install react router
+- add createBrowserRouter, Routes , route
+- create a body layout to add navbar routes and footer
+- add children route using outlet
 
-Currently, two official plugins are available:
+# Login page
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) (or [oxc](https://oxc.rs) when used in [rolldown-vite](https://vite.dev/guide/rolldown)) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## React Compiler
-
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+- create login page
+- add username and password field
+- install axios
+- make login api call with required details
+- install cors and add it as middleware for cors issue
+- make api calls with paramter credentials so that cookies are forward
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState("rohansingh@gmail.com");
+  const [password, setPassword] = useState("Rohan@123");
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId: username,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log("res---", res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm mx-auto my-20">
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">Login</h2>
+        <div className="flex flex-col gap-5 my-5">
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </g>
+            </svg>
+            <input
+              type="text"
+              required
+              placeholder="Username"
+              pattern="[A-Za-z][A-Za-z0-9\-]*"
+              minlength="3"
+              maxlength="30"
+              title="Only letters, numbers or dash"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              required
+              placeholder="Password"
+              minlength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div className="card-actions">
+          <button onClick={handleLogin} className="btn btn-primary">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
